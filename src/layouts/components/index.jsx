ec92098dbb81840b78bce57968dc8fbaaf3c0520
hainh <hainh.blackwind@gmail.com>
2022-12-14 15:03:42 +0700

    aa

--- src/layouts/components/index.jsx
+++ src/layouts/components/index.jsx
@@ -1,12 +1,9 @@
 // ** React Imports
 import React, { useState, useEffect } from 'react';
-import './style.scss';
+import './style.scss'
 import {
   MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined
+  MenuUnfoldOutlined
 } from '@ant-design/icons';
 import { Layout as LayoutAntd, Menu, theme } from 'antd';
 import NavbarLayout from './navbar';
@@ -32,55 +29,11 @@ const Layout = ({ menu, navbar, footer, menuData, children, routerProps, setLast
   }
   return (
     <LayoutAntd>
-      <Sider trigger={null}
-        collapsible
-        collapsed={collapsed}
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          bottom: 0,
+      <NavbarLayout collapsed={collapsed} />
+      <LayoutAntd className="site-layout" style={{ marginLeft: 200 }}>
+        <Header style={{
+          padding: 0, background: colorBgContainer, height: '5vh'
         }}
-      >
-        <div
-          style={{
-            height: 32,
-            margin: 16,
-            background: 'rgba(255, 255, 255, 0.2)',
-          }}
-        />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <LayoutAntd className="site-layout">
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-            height: '5vh'
-          }}
         >
           {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
             className: 'trigger',
@@ -93,12 +46,11 @@ const Layout = ({ menu, navbar, footer, menuData, children, routerProps, setLast
             padding: 8,
             height: '91vh',
             background: '#DEDEDE',
+            overflow: 'initial'
           }}>
           {children}
         </Content>
       </LayoutAntd>
-
-
     </LayoutAntd>
   )
 }
