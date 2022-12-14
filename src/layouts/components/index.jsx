a9ffa55e0e384238320a246c38af7285117d48fc
hainh <hainh.blackwind@gmail.com>
2022-12-14 14:32:03 +0700

    aa

--- src/layouts/components/index.jsx
+++ src/layouts/components/index.jsx
@@ -1,8 +1,12 @@
 // ** React Imports
 import React, { useState, useEffect } from 'react';
+import './style.scss';
 import {
   MenuFoldOutlined,
-  MenuUnfoldOutlined
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined
 } from '@ant-design/icons';
 import { Layout as LayoutAntd, Menu, theme } from 'antd';
 import NavbarLayout from './navbar';
@@ -10,13 +14,8 @@ const { Header, Sider, Content } = LayoutAntd;
 
 const Layout = ({ menu, navbar, footer, menuData, children, routerProps, setLastLayout, currentActiveItem }) => {
 
-
-
   const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
+  const { token: { colorBgContainer } } = theme.useToken();
 
   // ** States
   const [isMounted, setIsMounted] = useState(false)
@@ -33,7 +32,48 @@ const Layout = ({ menu, navbar, footer, menuData, children, routerProps, setLast
   }
   return (
     <LayoutAntd>
-      <NavbarLayout collapsed={collapsed} />
+      <Sider trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
       <LayoutAntd className="site-layout">
         <Header
           style={{
@@ -49,10 +89,10 @@ const Layout = ({ menu, navbar, footer, menuData, children, routerProps, setLast
         </Header>
         <Content
           style={{
-            margin: '16px',
+            margin: '8px',
             padding: 8,
             height: '91vh',
-            background: colorBgContainer,
+            background: '#DEDEDE',
           }}>
           {children}
         </Content>
