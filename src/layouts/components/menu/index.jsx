8864e68ba7d56ab0aab472227396d6b08afc2b50
hainh <hainh.blackwind@gmail.com>
2022-12-15 12:22:28 +0700

    aa

--- src/layouts/components/menu/index.jsx
+++ src/layouts/components/menu/index.jsx
@@ -4,50 +4,23 @@ import '../style.scss'
 import { AppstoreOutlined, MailOutlined, SettingOutlined, PlusCircleOutlined } from '@ant-design/icons'
 import { Layout as LayoutAntd, Menu } from 'antd'
 import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 const { Sider } = LayoutAntd
 
-
-function getItem(label, key, icon, children, type) {
-    return {
-        key,
-        icon,
-        children,
-        label,
-        type
-    }
-}
-
 const items = [
-    getItem('Navigation One', 'sub1', <MailOutlined />, [
-        getItem('Item 1', 'g1', <SettingOutlined />, [getItem('Option 1', '1'), getItem('Option 2', '2')]),
-        getItem('Item 2', 'g2', <SettingOutlined />, [getItem('Option 3', '3'), getItem('Option 4', '4')])
-    ]),
-    getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-        getItem('Option 5', '5', <AppstoreOutlined />),
-        getItem('Option 6', '6', <AppstoreOutlined />),
-        getItem('Submenu', 'sub3', <SettingOutlined />, [
-            getItem('Option 7', '7', <PlusCircleOutlined />),
-            getItem('Option 8', '8', <PlusCircleOutlined />, [
-                getItem('Option 71', '71', <PlusCircleOutlined />),
-                getItem('Option 81', '81', <PlusCircleOutlined />, [
-                    getItem('Option 7', '7', <PlusCircleOutlined />),
-                    getItem('Option 8', '8', <PlusCircleOutlined />)
-                ])
-            ])
-        ])
-    ]),
-    getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-        getItem('Option 9', '9', <PlusCircleOutlined />),
-        getItem('Option 10', '10', <PlusCircleOutlined />),
-        getItem('Option 11', '11', <PlusCircleOutlined />),
-        getItem('Option 12', '12', <PlusCircleOutlined />)
-    ]),
-    getItem('Navigation Three', 'sub5', <SettingOutlined />, [
-        getItem('Option 9', '9', <PlusCircleOutlined />),
-        getItem('Option 10', '10', <PlusCircleOutlined />),
-        getItem('Option 11', '11', <PlusCircleOutlined />),
-        getItem('Option 12', '12', <PlusCircleOutlined />)
-    ]),
+    {
+        key: '/users',
+        label: 'Nhân sự',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                key: 'list',
+                label: 'Quản lý nhân viên',
+                icon: <AppstoreOutlined />,
+                routes:['/user']
+            }
+        ]
+    }
 
 ]
 
